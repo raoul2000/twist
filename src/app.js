4720369fd8e4f076518b060354868d0e3b804293
raoul2000 <manu34@free.fr>
2019-01-09 00:07:53 +0100

    add save button

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ import Quill from '../node_modules/quill/dist/quill.js';
 import '../node_modules/quill/dist/quill.snow.css'
 import './style.css';
 
+// https://codepen.io/anon/pen/gLzmXo
 /**
  * @type {Quill} Quill instance
  */
 let quill = null;
 
 const initEditor = () => {
+    let saveButton = document.querySelector('.ql-save');
+
+    document.querySelector('.ql-save').addEventListener('click', function(e) {
+        console.log(quill.getContents());
+      });    
+    
     quill = new Quill('#quill-container', {
 
         modules: {
